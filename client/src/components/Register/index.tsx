b232260feece414bbe1463be9ff444444fe74720
mokunshao <1627510957@qq.com>
2019-05-20 14:20:59 +0800

    备份

--- client/src/components/Register/index.tsx
+++ client/src/components/Register/index.tsx
@@ -3,21 +3,23 @@ import styles from "./styles.module.scss";
 import { connect } from "react-redux";
 import { registerUser } from "../../redux/actions/authActions";
 
-interface Props{
-  registerUser:Function
+interface Errors {
+  username: "";
+  email: "";
+  password: "";
+  password2: "";
 }
 
-function Register(props:Props): React.ReactElement {
+interface Props {
+  registerUser: Function;
+  errors: Errors;
+}
+
+function Register(props: Props): React.ReactElement {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
-  const [errors, setErrors] = useState({
-    username: "",
-    email: "",
-    password: "",
-    password2: ""
-  });
   interface Iinput {
     target: {
       name: String;
@@ -56,54 +58,60 @@ function Register(props:Props): React.ReactElement {
       <form onSubmit={handleSubmit}>
         <div className={styles.inputArea}>
           <input
-            className={errors.username ? styles.invalid : ""}
+            className={props.errors.username ? styles.invalid : ""}
             name={"username"}
             value={username}
             onChange={handleChange}
             type="text"
             placeholder="用户名"
           />
-          {errors.username && (
-            <div className={styles.invalidFeedback}>{errors.username}</div>
+          {props.errors.username && (
+            <div className={styles.invalidFeedback}>
+              {props.errors.username}
+            </div>
           )}
         </div>
         <div className={styles.inputArea}>
           <input
-            className={errors.email ? styles.invalid : ""}
+            className={props.errors.email ? styles.invalid : ""}
             name={"email"}
             value={email}
             onChange={handleChange}
             type="email"
             placeholder="邮箱"
           />
-          {errors.email && (
-            <div className={styles.invalidFeedback}>{errors.email}</div>
+          {props.errors.email && (
+            <div className={styles.invalidFeedback}>{props.errors.email}</div>
           )}
         </div>
         <div className={styles.inputArea}>
           <input
-            className={errors.password ? styles.invalid : ""}
+            className={props.errors.password ? styles.invalid : ""}
             name={"password"}
             value={password}
             onChange={handleChange}
             type="password"
             placeholder="密码"
           />
-          {errors.password && (
-            <div className={styles.invalidFeedback}>{errors.password}</div>
+          {props.errors.password && (
+            <div className={styles.invalidFeedback}>
+              {props.errors.password}
+            </div>
           )}
         </div>
         <div className={styles.inputArea}>
           <input
-            className={errors.password2 ? styles.invalid : ""}
+            className={props.errors.password2 ? styles.invalid : ""}
             name={"password2"}
             value={password2}
             onChange={handleChange}
             type="password"
             placeholder="确认密码"
           />
-          {errors.password2 && (
-            <div className={styles.invalidFeedback}>{errors.password2}</div>
+          {props.errors.password2 && (
+            <div className={styles.invalidFeedback}>
+              {props.errors.password2}
+            </div>
           )}
         </div>
         <button onSubmit={handleSubmit}>注册</button>
@@ -112,9 +120,13 @@ function Register(props:Props): React.ReactElement {
   );
 }
 
+const mapStateToProps = (state: any) => ({
+  errors: state.errors
+});
+
 const mapDispatchToProps = { registerUser };
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(Register);
--- client/src/redux/reducers/errorReducer.tsx
+++ client/src/redux/reducers/errorReducer.tsx
@@ -1,9 +1,22 @@
 import { GET_ERRORS } from "../actionTypes";
-const initialState = {};
+
+const initialState = {
+  username: "",
+  email: "",
+  password: "",
+  password2: ""
+};
+
+interface Errors{
+  username: string,
+  email: string,
+  password: string,
+  password2: string
+}
 
 interface Action {
   type: string;
-  payload: any;
+  payload: Errors;
 }
 
 export default function(state = initialState, action: Action) {
