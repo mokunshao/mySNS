c2c138ee5a64bf658e1b910a4fea3effc85745cc
mokunshao <1627510957@qq.com>
2019-05-30 00:05:44 +0800

    完成 ProfileAbout 组件

--- client/src/components/ProfileAbout/index.tsx
+++ client/src/components/ProfileAbout/index.tsx
@@ -1,9 +1,34 @@
 import React from "react";
 import styles from "./styles.module.scss";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+library.add(faCheck);
 
 interface Props {
   data: any;
 }
 export default function ProfileAbout(props: Props) {
-  return <div className={styles.ProfileAbout}>ProfileAbout</div>;
+  const skills = props.data.skills.map((skill:string, index:number) => {
+    return (
+      <span key={index} className={styles.skill}>
+        <FontAwesomeIcon icon="check" />
+        <span>{skill}</span>
+      </span>
+    );
+  });
+  console.log(props.data.skills);
+  return (
+    <div className={styles.ProfileAbout}>
+      <h3>{props.data.user.username.trim().split(" ")[0]}个人简介</h3>
+      <p>
+        {props.data.bio
+          ? props.data.bio
+          : props.data.user.username + "没有填写介绍信息"}
+      </p>
+      <hr />
+      <h3>个人技能</h3>
+      <div>{skills}</div>
+    </div>
+  );
 }
